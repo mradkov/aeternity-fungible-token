e286dd23171e2fe2098bedf64571365a7da00253
Milen Radkov <milenradkov@me.com>
2019-07-26 14:34:16 +0300

    fix change_allowance trowing error due to an misplaced require

--- test/fungible-tokenTest.js
+++ test/fungible-tokenTest.js
@@ -126,4 +126,22 @@ describe('Fungible Token Contract', () => {
         assert.equal(get_allowance.decodedResult, 10);
     });
 
+    it('Fungible Token Contract: Increase Allowance', async () => {
+        const create_allowance = await contract.methods.create_allowance(otherKeypair.publicKey, 10);
+        assert.equal(topicHashFromResult(create_allowance), hashTopic('Allowance'));
+        assert.equal(Crypto.addressFromDecimal(create_allowance.result.log[0].topics[1]), ownerKeypair.publicKey);
+        assert.equal(Crypto.addressFromDecimal(create_allowance.result.log[0].topics[2]), otherKeypair.publicKey);
+        assert.equal(create_allowance.result.log[0].topics[3], 10);
+        assert.equal(create_allowance.result.returnType, 'ok');
+
+        const allowanceFailAmount = await contract.methods.create_allowance(otherKeypair.publicKey, -10).catch(e => e);
+        assert.include(allowanceFailAmount.decodedError, "NON_NEGATIVE_VALUE_REQUIRED");
+
+        const get_allowance = await contract.methods.allowance({from_account: ownerKeypair.publicKey, for_account: otherKeypair.publicKey}).catch(e => e);
+        assert.equal(get_allowance.decodedResult, 10);
+
+        const change_allowance = await contract.methods.change_allowance(otherKeypair.publicKey, 10).catch(e => e);
+        assert.equal(change_allowance.decodedError, 20);
+    });
+
 });
