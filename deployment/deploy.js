b09cb704a5e168d93d32fad69c41d32a1e6d9ae2
Milen Radkov <milenradkov@me.com>
2021-06-03 11:27:48 +0300

    feat(deploy): add proper deployer

--- deployment/deploy.js
+++ deployment/deploy.js
@@ -14,14 +14,49 @@
  *  OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
  *  PERFORMANCE OF THIS SOFTWARE.
  */
-const Deployer = require('forgae').Deployer;
+const {
+  Universal,
+  MemoryAccount,
+  Node,
+  Crypto,
+} = require('@aeternity/aepp-sdk');
+const contractUtils = require('../utils/contract-utils');
 
-const deploy = async (network, privateKey, compiler) => {
-    let deployer = new Deployer(network, privateKey, compiler)
+const NETWORKS = require('../config/network.json');
+const DEFAULT_NETWORK_NAME = 'local';
 
-    await deployer.deploy("./contracts/fungible-token.aes")
+const CONTRACT_SOURCE = './contracts/fungible-token.aes';
+
+const deploy = async (secretKey, network, compiler) => {
+  if (!secretKey) {
+    throw new Error(`Required option missing: secretKey`);
+  }
+  const KEYPAIR = {
+    secretKey: secretKey,
+    publicKey: Crypto.getAddressFromPriv(secretKey),
+  };
+  const NETWORK_NAME = network ? network : DEFAULT_NETWORK_NAME;
+
+  const client = await Universal({
+    nodes: [
+      {
+        name: NETWORK_NAME,
+        instance: await Node({ url: NETWORKS[NETWORK_NAME].nodeUrl }),
+      },
+    ],
+    compilerUrl: compiler ? compiler : NETWORKS[NETWORK_NAME].compilerUrl,
+    accounts: [MemoryAccount({ keypair: KEYPAIR })],
+    address: KEYPAIR.publicKey,
+  });
+  // a filesystem object must be passed to the compiler if the contract uses custom includes
+  const filesystem = contractUtils.getFilesystem(CONTRACT_SOURCE);
+  // get content of contract
+  const contract_content = contractUtils.getContractContent(CONTRACT_SOURCE);
+  contract = await client.getContractInstance(contract_content, { filesystem });
+  const deployment_result = await contract.deploy([]);
+  console.log(deployment_result);
 };
 
 module.exports = {
-    deploy
+  deploy,
 };
